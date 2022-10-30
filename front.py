eaad575d354ef30f0ff2c1c01413d2a9a6e2ceb5
Federico Cala <federico.cala@thehackettgroup.com>
2022-10-30 14:11:26 -0300

    Commit after Expo P

--- front.py
+++ front.py
@@ -369,3 +369,4 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    input("Press enter to proceed...")
---
+++ zapadapp.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\Federico\AppData\Local\Programs\Python\Python310\python.exe .\front.py
+pause
